389ae52c086ff76218e3e3542e1dcbd3c4c596fb
solanki-dev-1211 <solankidev1211@gmail.com>
2026-05-25 15:19:53 +0000

    conversion & opration

--- first/ConversionOprations.js
+++ first/ConversionOprations.js
@@ -16,3 +16,17 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(typeof stringNumber)
 console.log(stringNumber)   
+
+
+// oprations
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
