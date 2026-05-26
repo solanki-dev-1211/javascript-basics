4da5d40495dbdfd231109e922df9a34b04e8bfd9
solanki-dev-1211 <solankidev1211@gmail.com>
2026-05-26 09:38:18 +0000

    types of conversion

---
+++ first/4_conversion.js
@@ -0,0 +1,18 @@
+console.log(2 > 1)
+console.log(2 <= 1)
+console.log(2 >= 1)
+console.log(2 == 1)
+console.log(2 != 1)
+console.log(2 < 1)
+
+// avoide this type conversion
+console.log("2" > 1)
+console.log("03"> 1)
+
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+// === strick check check data type etc..
+console.log("2" === 2)
