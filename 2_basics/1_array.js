aa68451e6a1279b9ab871ab16c2af7fbe16d9a7b
solanki-dev-1211 <solankidev1211@gmail.com>
2026-05-28 12:27:59 +0000

    array

---
+++ 2_basics/1_array.js
@@ -0,0 +1,8 @@
+// array
+const myarr=[0,1,2,3,4,5]
+const Frutes=['banana','mango','apple']
+const Cartoon=['ben10','chottabheem','doremone','shinchin']
+
+console.log(myarr[3])
+console.log(Frutes)
+console.log(Cartoon[0])
